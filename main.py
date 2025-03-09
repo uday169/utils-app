540a34bf91ee55a9b1dcbf81e4e0724a0eaffddf
Uday Pratap <udaypratap169@gmail.com>
2025-03-09 22:53:26 +0530

    updated images

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # main.py
 from fastapi import FastAPI
 from app.routers import home_router, about_router, base64_router
+from fastapi.staticfiles import StaticFiles
 
 # Initialize the FastAPI app
 app = FastAPI()
+
+# Serve static files
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 # Include routers
 app.include_router(home_router)
